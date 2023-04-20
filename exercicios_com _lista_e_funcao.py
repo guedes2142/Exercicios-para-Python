ac5f128f643f9bdd98625c52f4f58b2be51b653e
guedes2142 <rafatattoox@gmail.com>
2023-04-20 15:19:42 -0300

    first commit

---
+++ exercicios_com _lista_e_funcao.py
@@ -0,0 +1,52 @@
+'''
+Escreva uma função em Python que recebe um número como parâmetro e retorna 
+o dobro desse número. Em seguida, escreva um programa que pede ao usuário 
+para digitar um número, chama a função para calcular o dobro do número digitado e exibe 
+o resultado na tela.
+'''
+
+
+
+'''
+Escreva uma função em Python que recebe uma lista de números como parâmetro e 
+retorna a média desses números. Em seguida, escreva um programa que pede ao usuário 
+para digitar uma lista de números separados por vírgulas, 
+chama a função para calcular a média desses números e exibe o resultado na tela.
+Sim! A fórmula para calcular a média de uma lista de números é simples: basta somar todos os 
+elementos da lista e dividir o resultado pelo número de elementos na lista. Matematicamente, 
+a fórmula para calcular a média pode ser escrita da seguinte forma:
+
+Média = (x1 + x2 + ... + xn) / n
+
+Onde:
+
+    x1, x2, ..., xn são os elementos da lista de números.
+    n é o número de elementos na lista.
+'''
+
+
+
+"""
+Escreva uma função chamada soma_quadrados que recebe dois números 
+inteiros como parâmetros e retorna a soma dos seus quadrados. Em seguida
+escreva um programa que solicita que o usuário digite dois números inteiros,
+chama a função soma_quadrados com esses números e exibe o resultado.
+
+#formula 
+
+a² + b²
+onde "a" e "b" são os números que queremos elevar ao quadrado e somar.
+
+#exemplo de saida
+
+Digite um número inteiro: 3
+Digite outro número inteiro: 4
+A soma dos quadrados de 3 e 4 é 25.
+
+"""
+
+
+'''
+Escreva um programa que recebe uma lista de números
+inteiros como entrada e retorna a soma dos elementos da lista.
+'''
